4d7eedf87aaaced0a86c35798dbda3dd46e2128c
Devanshu Saxena <86067292+devanshu0605@users.noreply.github.com>
2023-08-04 16:03:15 +0530

    internship demo

---
+++ constants/index.js
@@ -0,0 +1,88 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-01.png',
+    title: 'AR/VR',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: 'WEB DEVELOPMENT',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-03.png',
+    title: 'ANDROID DEVELOPMENT',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'GRAPHICS DESIGNING',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'MACHINE LEARNING',
+  },
+];
+
+export const startingFeatures = [
+  'Custom Online School - The Coursify.me has an easy to use builder for you to create your online school in a few steps.',
+  'Online courses builder - Create your online courses with an easy and intuitive builder, which allows you to view how your course will look in real time.',
+  'Student area - Easy customization of the visual identity of your online courses with logo, images and custom color scheme.',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: ': Custom Online School',
+    subtitle:
+        'The Coursify.me has an easy to use builder for you to create your online school in a few steps.',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: ': Online courses builder',
+    subtitle:
+        'Create your online courses with an easy and intuitive builder, which allows you to view how your course will look in real time. ',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: 'Customize your school',
+    subtitle:
+        'Enter your logo, images, colors and style to create an online school with your identity in a few minutes.',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: 'Create your courses',
+    subtitle:
+        'Import text, images, videos and other content from Dropbox, YouTube, Vimeo or from your computer.',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'Reach your audience',
+    subtitle:
+        'Provide access to your courses to anywhere in the world easily and securely.',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+    link: 'https://twitter.com/'
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+    link: 'https://www.linkedin.com/company/'
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+    link: 'https://www.instagram.com/'
+  },
+  
+];
---
+++ sections/About.jsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import { TypingText } from '../components';
+
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
+
+const About = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0" />
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+      <TypingText title="| About Coursify" textStyles="text-center" />
+
+      <motion.p
+        variants={fadeIn('up', 'tween', 0.2, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
+      >
+        <span className="font-extrabold text-white">Create your online courses </span>  in a few steps!
+        our courses. Your content. Your audience. .{' '}
+        <span className="font-extrabold text-white">
+        You can customize the look of your courses quickly by choosing a color scheme, putting your logo your cover image and links to their social networks.
+        </span>{' '}
+    
+        <span className="font-extrabold text-white"></span>You can also manage your courses and to offer its students an optimized learning environment, with progress control and support each lesson via comments to ask questions.    {' '}
+   
+      </motion.p>
+
+      <motion.img
+        variants={fadeIn('up', 'tween', 0.3, 1)}
+        src="/arrow-down.svg"
+        alt="arrow down"
+        className="w-[18px] h-[28px] object-contain mt-[28px]"
+      />
+    </motion.div>
+  </section>
+);
+
+export default About;
